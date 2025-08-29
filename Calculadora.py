4be1154738290e42a28fb6d6c0d2792967a56c56
Zharickrodrigue <zharick2809.com>
2025-08-29 17:03:37 -0500

    multiplicacion

--- Calculadora.py
+++ Calculadora.py
@@ -8,6 +8,8 @@ print("4. División")
 
 opcion = input("Ingresa el número de la operación (1/2/3/4): ")
 
-   
-
-      
+if opcion == 3:
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    resultado = num1 * num2
+    print("El resultado de la multiplicación es:", resultado)
