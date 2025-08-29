f931e2c8db5743438cc9fabf595fcead39385fe4
Zharickrodrigue <zharick2809.com>
2025-08-29 17:15:18 -0500

    Archivo division

--- Calculadora.py
+++ Calculadora.py
@@ -8,6 +8,12 @@ print("4. División")
 
 opcion = input("Ingresa el número de la operación (1/2/3/4): ")
 
-   
-
-      
+if opcion == 4:
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    if num2 != 0:
+        resultado = num1 / num2
+        print("El resultado de la división es:", resultado)
+    else:
+        print("Error: División por cero no permitida.")
+        
